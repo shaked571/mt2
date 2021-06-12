8a478b5fbcc94a5f073c795c8e15c493e9493afa
shaked571 <shakedgreenfeld@gmail.com>
2021-06-12 22:14:51 +0300

    add training and stuff

--- models.py
+++ models.py
@@ -1,112 +1,23 @@
-import math
-
 import torch.nn as nn
 import torch
 import numpy as np
 from vocab import Vocab
 
 
-# class EncoderAttention(nn.Module):
-#     def __init__(self, vocab_size, embed_size, hidden_size,
-#                  n_layers=1, dropout=0.5):
-#         super(EncoderAttention, self).__init__()
-#         self.vocab_size = vocab_size
-#         self.hidden_size = hidden_size
-#         self.embed_size = embed_size
-#         self.n_layers = n_layers
-#
-#         self.embed = nn.Embedding(vocab_size, embed_size, padding_idx=0)
-#         self.gru = nn.GRU(embed_size, hidden_size, n_layers,
-#                           dropout=dropout, bidirectional=True)
-#
-#         self.dropout = nn.Dropout(dropout)
-#
-#
-#     def forward(self, src, hidden=None):
-#         embedded = self.embed(src)
-#         outputs, hidden = self.gru(embedded, hidden)
-#         # sum bidirectional outputs
-#         outputs = (outputs[:, :, :self.hidden_size] +
-#                    outputs[:, :, self.hidden_size:])
-#         return outputs, hidden
-
-
-# class Attention(nn.Module):
-#     def __init__(self, hidden_size):
-#         super(Attention, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
-#         self.v = nn.Parameter(torch.rand(hidden_size))
-#         self.tanh = nn.Tanh()
-#         self.Wa = nn.Linear(hidden_size * 2, hidden_size, bias=False)
-#         self.Ua = nn.Linear(hidden_size * 2, hidden_size, bias=False)
-#         # self.va = nn.Parameter(torch.FloatTensor(batch_size, hidden_size))
-#         #
-#         self.softmax = nn.Softmax(dim=1)
-#         stdv = 1. / math.sqrt(self.v.size(0)) #init like the paper
-#         self.v.data.uniform_(-stdv, stdv)
-#
-#     def forward(self, hidden, encoder_outputs):
-#         timestep = encoder_outputs.size(0)
-#         h = hidden.repeat(timestep, 1, 1).transpose(0, 1)
-#         encoder_outputs = encoder_outputs.transpose(0, 1)  # [B*T*H]
-#         attn_energies = self.score(h, encoder_outputs)
-#         return self.softmax(attn_energies).unsqueeze(1)
-#
-#     # def score(self,):
-#     #     x = last_hidden.unsqueeze(1)
-#     #     out = self.tanh(self.Wa(x) + self.Ua(encoder_outputs))
-#     #     return out.bmm(self.va.unsqueeze(2)).squeeze(-1)
-#
-#
-#
-#     def score(self, hidden, encoder_outputs):
-#             # [B*T*2H]->[B*T*H]
-#             energy = self.relu(self.attn(torch.cat([hidden, encoder_outputs], 2)))
-#             energy = energy.transpose(1, 2)  # [B*H*T]
-#             v = self.v.repeat(encoder_outputs.size(0), 1).unsqueeze(1)  # [B*1*H]
-#             energy = torch.bmm(v, energy)  # [B*1*T]
-#             return energy.squeeze(1)  # [B*T]
-
 
 class Attention(nn.Module):
-    # """ Applies attention mechanism on the `context` using the `query`.
-    #
-    # **Thank you** to IBM for their initial implementation of :class:`Attention`. Here is
-    # their `License
-    # <https://github.com/IBM/pytorch-seq2seq/blob/master/LICENSE>`__.
-    #
-    # Args:
-    #     dimensions (int): Dimensionality of the query and context.
-    #     attention_type (str, optional): How to compute the attention score:
-    #
-    #         * dot: :math:`score(H_j,q) = H_j^T q`
-    #         * general: :math:`score(H_j, q) = H_j^T W_a q`
-    #
-    # Example:
-    #
-    #      attention = Attention(256)
-    #      query = torch.randn(5, 1, 256)
-    #      context = torch.randn(5, 5, 256)
-    #      output, weights = attention(query, context)
-    #      output.size()
-    #      torch.Size([5, 1, 256])
-    #      weights.size()
-    #      torch.Size([5, 1, 5])
-    # """
 
     def __init__(self, hidden_dim):
         super(Attention, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.concat_linear = nn.Linear(self.hidden_dim * 2 * 2, self.hidden_dim)
-        self.attn = nn.Linear(self.hidden_dim * 2, self.hidden_dim * 2)
+        self.hidden_dim = hidden_dim * 2  # We are working with bi-directional
+        self.attn = nn.Linear(self.hidden_dim, self.hidden_dim)
         self.softmax = nn.Softmax(dim=1)
         self.tanh = nn.Tanh()
 
     def forward(self, rnn_outputs, final_hidden_state):
-        # rnn_output.shape:         (seq_len, batch_size , hidden_size)
-        # final_hidden_state.shape: (batch_size, hidden_size)
-        # NOTE: hidden_size may also reflect bidirectional hidden states (hidden_size = num_directions * hidden_dim)
+        # rnn_output.shape:         (B, seq_len,  H)
+        # final_hidden_state.shape: (B, H)
+        # hidden_size may also reflect bidirectional hidden states (hidden_size = num_directions * hidden_dim)
         rnn_outputs = rnn_outputs.transpose(0, 1)
         batch_size, seq_len, _ = rnn_outputs.shape
         attn_weights = self.attn(rnn_outputs) # (batch_size, seq_len, hidden_dim)
@@ -142,21 +53,19 @@ class DecoderAttention(nn.Module):
 
         #Get the hidden size from the layers and directions
         final_state = hidden.view(self.n_layers, self.num_directions, batch_size, self.hidden_size)[-1]
-
         #Because num of directions is always 2 now we need to ge both directions
         h_1, h_2 = final_state[0], final_state[1]
         # final_hidden_state = h_1 + h_2  -> Add both states (requires changes to the input size of first linear layer + attention layer)
         final_hidden_state = torch.cat((h_1, h_2), 1)  # Concatenate both states
 
         attn_weights = self.attention(encoder_outputs, final_hidden_state)
-        context = torch.bmm(encoder_outputs.transpose(0, 1).transpose(1, 2), attn_weights.unsqueeze(2)).squeeze(2)
-        # attn_hidden = torch.cat((context, final_hidden_state), dim=1)
+        context = torch.bmm(encoder_outputs.transpose(0, 1).transpose(1, 2), attn_weights.unsqueeze(2)).squeeze(2) #B * (H * Num_of_dirc)
         # Combine embedded input word and attended context, run through RNN
-        rnn_input = torch.cat([embedded, context], 1)
-        output,  (hidden, cell) = self.lstm(rnn_input.unsqueeze(0),  (hidden, cell))
+        rnn_input = torch.cat([embedded, context], 1) # rnn_input B * (embedded + context)
+        output,  (hidden, cell) = self.lstm(rnn_input.unsqueeze(0),  (hidden, cell)) #hidden ->
         output = output.squeeze(0)  # (1,B,N) -> (B,N)
         context = context.squeeze(0) if len(context.shape) == 3 else context #dealing with batch size 1
-        output = self.out_linear(torch.cat([output, context], 1))
+        output = self.out_linear(torch.cat([output, context], 1)) #
         return output, hidden, attn_weights
 
 
@@ -189,7 +98,7 @@ class DecoderVanilla(nn.Module):
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
         self.embed_size = embed_size
-
+        self.dropout = nn.Dropout(p=dropout)
         self.n_layers = n_layers
         self.embedding = nn.Embedding(self.vocab_size, embed_size * 2, padding_idx=0)
         self.lstm = nn.LSTM(embed_size * 2, hidden_size, n_layers,bidirectional=True,
@@ -201,6 +110,7 @@ class DecoderVanilla(nn.Module):
     def forward(self, input_tok, hidden, cell):
         # input is the previous predicted word
         output = self.embedding(input_tok)
+        output = self.dropout(output)
         output = self.relu(output)
         output, (hidden, cell) = self.lstm(output.unsqueeze(0), (hidden, cell))
         output = self.out_linear(output) #  N * 1 * lenght_of_vocab
@@ -235,7 +145,7 @@ class Seq2Seq(nn.Module):
             if type(self.decoder) == DecoderVanilla:
                 output, (hidden, cell) = self.decoder(
                     output, hidden, cell)
-            else: #attention
+            else:  # Attention
                 output, hidden, attn_weights = self.decoder(
                     output, hidden,cell, encoder_output)
             outputs.append(output)
@@ -244,7 +154,7 @@ class Seq2Seq(nn.Module):
             else:
                 output[:, 0] = -100  # Dont allow prediction of Padding index.
                 output = output.argmax(1)
-                if all([i == Vocab.END_IDX for i in output.cpu().numpy()]): # Support Dev only for batch in size of 1
+                if all([i == Vocab.END_IDX for i in output.cpu().numpy()]):  # Support Dev only for batch in size of 1
                     break
 
         outputs = torch.stack(outputs, dim=1)
---
+++ part2_evaluation.py
@@ -0,0 +1,29 @@
+from torch.utils.data import DataLoader
+
+from data_sets import TranslationDataSet
+from trainer import Trainer, set_seed, pad_collate
+from part2_train import model, data_path, source_vocab, target_vocab, batch_size, lr
+
+
+def main():
+    set_seed(42)
+    print(model)
+    train_df = TranslationDataSet(source=data_path.format("train", "src"), target=data_path.format("train", "trg"),
+                                  source_vocab=source_vocab, target_vocab=target_vocab)
+    dev_df = TranslationDataSet(source=data_path.format("dev", "src"), target=data_path.format("dev", "trg"),
+                                source_vocab=source_vocab, target_vocab=target_vocab)
+
+    trainer = Trainer(model=model, train_data=train_df, dev_data=dev_df, source_vocab=source_vocab,
+                      target_vocab=target_vocab, lr=lr, train_batch_size=batch_size, out_path="part2_model")
+
+
+    test_df = TranslationDataSet(source=data_path.format("test", "src"), target=data_path.format("test", "trg"),
+                                 source_vocab=source_vocab, target_vocab=target_vocab)
+
+    test_dateset = DataLoader(test_df, batch_size=trainer.dev_batch_size, collate_fn=pad_collate)
+
+    trainer.evaluate_model(1, "test", test_dateset, load_model=True, write=False)
+
+
+if __name__ == '__main__':
+    main()
